4347df58470285f818608f220de5177a4b6fd63e
Mian Tabusam <tabusam@ibxtechnologies.com>
2019-04-24 19:21:19 +0500

    adding to github

---
+++ articles/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .models import articles
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .import forms
+# Create your views here.
+
+
+def article_list(request):
+
+    article = articles.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': article})
+
+
+def article_detail(request, slug):
+
+     #return HttpResponse(slug)
+
+     article = articles.objects.get(slug=slug)
+     return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    if request.method == 'POST':
+      form = forms.CreateArticle(request.POST, request.FILES)
+      if form.is_valid():
+
+
+          instance = form.save(commit=False)
+          instance.author = request.user
+          instance.save()
+          return redirect('articles:list')
+    else:
+      form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form': form})
+
